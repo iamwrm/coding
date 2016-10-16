89535504f86fb26897e380938a0e2611a59285c7
Ren Wang <iamwrm@gmail.com>
2016-10-16 14:47:09 +0800

    q1 完成

--- 161015_mat_vg101_hw2/myPi.m
+++ 161015_mat_vg101_hw2/myPi.m
@@ -1,26 +1,27 @@
-function [n,PI]=myPi()
+% calculate pi by Monte Carlo method
+function [n,PI] = myPi()
+% N is the number of dots in the square
+% put N dots into a 1x1 square
 clc
+
 fprintf('This program will estimate the value of PI\n');
 while 1
-
 n=input('input an integer (-1 for quit)-->','s');
 n=str2num(n);
 if n<0
 	break;
 end
-% r=randi([0 1],n,n);
-r=ones(n);
 
-my_sum=0;
-for i1=1:n
-	for i2=1:n
-		if (r(i1,i2)==1)&&((i1)^2+(i2)^2<=n^2)
-			my_sum=my_sum+1;
-		end
-	end
+sum = 0; % number of dots in the fan
+for ii = 1:n
+    % random numbers ==> dot position
+    dotPos = rand(1, 2);
+    % if the dot is in the fan
+    if sqrt(dotPos(1).^2 + dotPos(2).^2) < 1
+        sum = sum + 1;
+    end
 end
-
-PI=my_sum/n^2*8;
+PI = sum / n * 4;
+fprintf(['PI is estimated as ',num2str(PI),' for ',num2str(n), ' dots\n\n']);
 
 end
-    
