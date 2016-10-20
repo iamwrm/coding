52e1ee40c4fd70ce50a6ff5593815c6c10bc088d
Ren Wang <iamwrm@gmail.com>
2016-10-20 10:17:11 +0800

    改了一下变量名

--- 161018_mat_vg101_lecture/my_tri.m
+++ 161018_mat_vg101_lecture/my_tri.m
@@ -1,15 +1,15 @@
 % my_tri: function description
 % point is a 2 x 4 matrix
-function my_tri(point1,point2,point3,lev,n)
+function my_tri(p1,p2,pt3,lev,n)
 	if lev>=n
 		return;
 	end
 
-plot([point1(1),point2(1),point3(1),point1(1)],[point1(2),point2(2),point3(2),point1(2)],'b');
+plot([p1(1),p2(1),pt3(1),p1(1)],[p1(2),p2(2),pt3(2),p1(2)],'b');
 
-my_tri((point1+point3)/2,(point2+point3)/2,point3,lev+1,n);
-my_tri(point1,(point1+point2)/2,(point1+point3)/2,lev+1,n);
-my_tri((point1+point2)/2,point2,(point2+point3)/2,lev+1,n);
+my_tri((p1+pt3)/2,(p2+pt3)/2,pt3,lev+1,n);
+my_tri(p1,(p1+p2)/2,(p1+pt3)/2,lev+1,n);
+my_tri((p1+p2)/2,p2,(p2+pt3)/2,lev+1,n);
 
 
 
