feafb3970b3477d4e9aa5626948fe6256650e3d6
Ren Wang <iamwrm@gmail.com>
2016-10-22 17:05:17 +0800

    c finish

--- 161022_mat_vg101_midterm1_Fall2015/test2_3.m
+++ 161022_mat_vg101_midterm1_Fall2015/test2_3.m
@@ -1,36 +1,7 @@
-clear
-clc
-
 nld=input('Please input a number ','s');% nld = number of lots of digits
 
 
 nld_len=length(nld);
-nldns=0;
-for i1=1:nld_len
-	nldns=nldns+str2num(nld(1,i1));
-end
-disp(nldns);
-
-
-%------------------------
-
-while 1
-nld_len=length(nld);
-nldns=0;
-for i1=1:nld_len
-	nldns=nldns+str2num(nld(1,i1));
-end
-if nldns<10
-	break;
-end
-
-nld=num2str(nldns);
-end
-disp(nldns)
-
-
-%----------
-
 
 ifodd=mod(nld_len,2);
 %totalpairs=(nld_len-ifodd)/2+1;
@@ -53,8 +24,6 @@ if ifodd==1
 	nlds31=nlds31+str2num(nld(end));
 	disp(nlds31);
 end
-
-
 
 
 
