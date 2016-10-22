fc80633a1de82c0d5e9316aa1d6ab5389ba58829
Ren Wang <iamwrm@gmail.com>
2016-10-22 08:58:52 +0800

    written the scripts with sufficient comments, but it doesn't work

--- 161021_mat_vg101_lab3/test7.m
+++ 161021_mat_vg101_lab3/test7.m
@@ -4,11 +4,11 @@ clc
 % not perfectly solve the problem
 
 [op,colormap_op]=imread('l3.bmp');%		op stands for original picutre
-cp=imread('l3176.bmp');	%	cp stands for classified picture
+cp=imread('l3120.bmp');	%	cp stands for classified picture
 anumber=imread('9.bmp');
 
 ops=[];
-cps=[];%	classified picutre stored
+cps=[];%	classified picutre stored that has been deleted the same row
 
 
 for i1=1:1920
@@ -17,6 +17,22 @@ for i1=1:1920
 		cps=[cps;cp(i1,:)];
 	end
 end
+
+opsc=[];%classified picutre stored that has been
+		% deleted the same row and the same column
+cpsc=[];
+
+for i1=1:1080
+	if ~isequal(ops(:,i1),cps(:,i1))
+		opsc=[opsc,ops(:,i1)];
+		cpsc=[cpsc,cps(:,i1)];
+	end
+end
+
+cpscr=reshape(cpsc,[192 76]);
+
+imshow(cpscr);
+
 
 
 
