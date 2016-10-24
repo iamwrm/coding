ab1f3e85c3f110ee01d4fd13952c7302bba71b85
Ren Wang <iamwrm@gmail.com>
2016-10-24 16:49:32 +0800

    hanoi problem

---
+++ 161024_mat_vg101_midterm_review/hanoi.m
@@ -0,0 +1,16 @@
+function hanoi(n)
+%a上原有盘数为n的汉诺塔。
+han(n,'a','b','c')
+
+function han(n,a,b,c)
+if n==1
+ex(a,c)
+else
+han(n-1,a,c,b)
+ex(a,c)
+han(n-1,b,a,c)
+end
+
+function ex(a,b)
+disp ([a '-->' b])
+
