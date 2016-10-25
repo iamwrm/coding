9240f45085a8049b8eb1722fb22ef62cc9e5365c
Ren Wang <iamwrm@gmail.com>
2016-10-25 13:14:37 +0800

    Revert "文件读写的模板"
    This reverts commit 93b63b47964f5ff87276c52bf1a380eb3505256a.

--- 161024_mat_vg101_midterm_review/sprintf_struct.m
+++
@@ -1,9 +0,0 @@
-clear
-clc;
-TA(1) = struct('name','M','value',1);
-TA(2) = struct('name','S','value',2);
-TA(3) = struct('name','Y','value',3);
-disp(sprintf('name\tvalue'));
-for i=1:3
-    disp(sprintf('%s\t\t%d',TA(i).name,TA(i).value));
-end
