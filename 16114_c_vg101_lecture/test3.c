8bd4b437f923d86dc844d365095b7b2afaaa04a6
Ren Wang <iamwrm@gmail.com>
2016-11-04 15:27:00 +0800

    遍历查找，还不知道怎么写二分，是用迭代还是递归？

--- 16114_c_vg101_lecture/test3.c
+++ 16114_c_vg101_lecture/test3.c
@@ -2,20 +2,33 @@
 #include "stdio.h"
 #include "math.h"
 #include "stdlib.h"
-#define Nmax 3
+#define Nmax 20
 void find(int* array,int a) {
   /* code */
+int i=0;
+
+for(i=0;i<Nmax;i++){
+  if (array[i]==a) {
+    printf("the index is %d\n",i );
+    break;}
+
+  if (i==Nmax-1) printf("Can't find\n" );
+}
+
+
+
 }
 
 
 int main(int argc, char const *argv[]) {
   /* code */
 
-  int* array;
-  array=(int *)malloc(sizeof(int)*4*Nmax);
-  array[0]=0;
+  int array[Nmax]={0,1,2,3,4,5,23,34,42,56,67,90,110};
+  /*array[0]=0;
   array[1]=1;
-  array[2]=2;
-  for(int i=0;i<Nmax;i++) printf("%d\n", array[i]);
+  array[2]=2;*/
+  int a=36;
+  find(array,a);
+  //for(int i=0;i<Nmax;i++) printf("%d\n", array[i]);
   return 0;
 }
