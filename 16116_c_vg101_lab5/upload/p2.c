b0e418141911ee11cc9f9da7d0dca1fefa4f57ae
Ren Wang <iamwrm@gmail.com>
2016-11-10 12:59:55 +0800

    为了hc， 修改一下代码

---
+++ 16116_c_vg101_lab5/upload/p2.c
@@ -0,0 +1,116 @@
+/* GNU gcc compiler on windows 10 x64 platform
+mingw32  gcc version 4.9.2
+ -std=c11					*/
+
+#include "stdio.h"
+#include "stdlib.h"
+#include "math.h"
+#include "ctype.h"
+
+
+#define MAXOP 100
+#define NUMBER '0'
+#define MAXVAL 100
+
+int sp=0;
+double val[MAXVAL];
+char buf[100];
+int bufp=0;
+
+
+int getop(char[]);
+void push(double);
+double pop(void);
+int getch(void);
+void ungetch(int);
+
+
+
+
+int main(int argc, char const *argv[]) {
+    int type;
+    double op2;
+    char s[MAXOP];//1,2,-,4,5,+,*
+    //    7,4,2,1,-,*,5,+,-,3,*
+
+    while ((type=getop(s))!=EOF){
+
+
+
+        switch (type) {
+            case NUMBER:
+                push(atof(s));
+                break;
+            case '+':
+                push(pop()+pop());
+                break;
+            case '*':
+                push(pop()*pop());
+                break;
+            case '-':
+                op2=pop();
+                push(pop()-op2);
+                break;
+            case '/':
+                op2=pop();
+                push(pop()/op2);
+                break;
+            case '\n':
+                printf("%8g\n", pop());
+                break;
+            default:
+                printf("error: unknown command %s\n", s);
+                break;
+
+        }
+
+
+    }
+    return 0;
+}
+
+void push(double f){
+
+        val[sp++]=f;
+
+}
+
+double pop(void){
+    if (sp>0)
+        return val[--sp];
+    else {
+        printf("stack empty\n" );
+        return 0.0;
+    }
+
+}
+
+int getop(char s[]){
+    int i,c;
+    while ((s[0]=c=getch())==',' || c== '\t');
+    s[1]='\0';
+    if (!isdigit(c) && c!='.')
+        return c;
+    i=0;
+    if (isdigit(c))
+        while (isdigit(s[++i]=c=getch()));
+    if (c=='.')
+        while (isdigit(s[++i]=c=getch()));
+    s[i]='\0';
+    if (c != EOF)
+        ungetch(c);
+    return NUMBER;
+
+
+}
+
+int getch(void){
+    return (bufp>0)? buf[--bufp] :  getchar();
+}
+
+void ungetch(int c){
+    if (bufp>= 100)
+        printf("ungetch: too many characters\n");
+    else
+        buf[bufp++] =c;
+}
---
+++ 16116_c_vg101_lab5/upload/p3.c
@@ -0,0 +1,186 @@
+#include "string.h"
+#include "string.h"
+#include <stdbool.h>
+// 操作符
+// 优先级		符号		运算顺序
+// 1		!		从右至左
+// 2		* / %		从左至右
+// 3		+ -		从左至右
+// 4		=		从右至左
+int op_preced(const char c)
+{
+    switch(c)    {
+        case '*':  case '/':
+            return 3;
+        case '+': case '-':
+            return 2;
+    }
+    return 0;
+}
+
+unsigned int op_arg_count(const char c)
+{
+            return 2;
+}
+
+#define op_left_assoc(c) (c == '+' || c == '-' || c == '/' || c == '*' || c == '%')
+#define is_operator(c)   (c == '+' || c == '-' || c == '/' || c == '*' || c == '!' || c == '%' || c == '=')
+#define is_function(c)   (c >= 'A' && c <= 'Z')
+#define is_ident(c)      ((c >= '0' && c <= '9') || (c >= 'a' && c <= 'z'))
+
+bool shunting_yard(const char *input, char *output)
+{
+    const char *strpos = input, *strend = input + strlen(input);
+    char c, stack[32], sc, *outpos = output;
+    unsigned int sl = 0;
+    while(strpos < strend)   {
+        c = *strpos;
+        if(c != ' ')    {
+            // 如果输入为一个数字，则直接入输出队列
+            if(is_ident(c))  {
+                *outpos = c; ++outpos;
+                *outpos=',';
+                ++outpos;
+            }
+            // 如果输入符号为操作符，op1，然后:
+            else if(is_operator(c))  {
+                while(sl > 0)    {
+                    sc = stack[sl - 1];
+                    // While there is an operator token, o2, at the top of the stack
+                    // op1 is left-associative and its precedence is less than or equal to that of op2,
+                    // or op1 is right-associative and its precedence is less than that of op2,
+                    if(is_operator(sc) &&
+                        ((op_left_assoc(c) && (op_preced(c) <= op_preced(sc))) ||
+                           (!op_left_assoc(c) && (op_preced(c) < op_preced(sc)))))   {
+                        // Pop o2 off the stack, onto the output queue;
+                        *outpos = sc;
+                        ++outpos;
+                        *outpos=',';
+                        ++outpos;
+                        sl--;
+                    }
+                    else   {
+                        break;
+                    }
+                }
+                // push op1 onto the stack.
+                stack[sl] = c;
+                ++sl;
+            }
+            // If the token is a left parenthesis, then push it onto the stack.
+            else if(c == '(')   {
+                stack[sl] = c;
+                ++sl;
+            }
+            // If the token is a right parenthesis:
+            else if(c == ')')    {
+                bool pe = false;
+                // Until the token at the top of the stack is a left parenthesis,
+                // pop operators off the stack onto the output queue
+                while(sl > 0)     {
+                    sc = stack[sl - 1];
+                    if(sc == '(')    {
+                        pe = true;
+                        break;
+                    }
+                    else  {
+                        *outpos = sc;
+                        ++outpos;
+                        *outpos=',';
+                        ++outpos;
+                        sl--;
+                    }
+                }
+                // Pop the left parenthesis from the stack, but not onto the output queue.
+                sl--;
+                // If the token at the top of the stack is a function token, pop it onto the output queue.
+
+
+
+            }
+        }
+        ++strpos;
+    }
+    // When there are no more tokens to read:
+    // While there are still operator tokens in the stack:
+    while(sl > 0)  {
+        sc = stack[sl - 1];
+        if(sc == '(' || sc == ')')   {
+            printf("Error: parentheses mismatched\n");
+            return false;
+        }
+        *outpos = sc;
+        ++outpos;
+        *outpos=',';
+        ++outpos;
+
+        --sl;
+    }
+    *outpos = 0; // Null terminator
+    return true;
+}
+
+bool execution_order(const char *input) {
+    const char *strpos = input, *strend = input + strlen(input);
+    char c, res[4];
+    unsigned int sl = 0, sc, stack[32], rn = 0;
+	// While there are input tokens left
+    while(strpos < strend)  {
+		// Read the next token from input.
+        c = *strpos;
+		// If the token is a value or identifier
+        if(is_ident(c))    {
+			// Push it onto the stack.
+            stack[sl] = c;
+            ++sl;
+        }
+		// Otherwise, the token is an operator  (operator here includes both operators, and functions).
+        else if(is_operator(c) || is_function(c))    {
+			++rn;
+			// It is known a priori that the operator takes n arguments.
+			unsigned int nargs = op_arg_count(c);
+			// If there are fewer than n values on the stack
+			if(sl < nargs) {
+				// (Error) The user has not input sufficient values in the expression.
+				return false;
+			}
+			// Else, Pop the top n values from the stack.
+			// Evaluate the operator, with the values as arguments.
+			// Push the returned results, if any, back onto the stack.
+            stack[sl] = *(unsigned int*)res;
+            ++sl;
+        }
+        ++strpos;
+    }
+	// If there is only one value in the stack
+	// That value is the result of the calculation.
+	if(sl == 1) {
+		sc = stack[sl - 1];
+		sl--;
+	//	printf("%s is a result\n", &sc);
+		return true;
+	}
+	// If there are more values in the stack
+	// (Error) The user input has too many values.
+	return false;
+}
+
+int main() {
+    // functions: A() B(a) C(a, b), D(a, b, c) ...
+    // identifiers: 0 1 2 3 ... and a b c d e ...
+    // operators: = - + / * % !
+    char iii[100];
+    printf("input: ");
+    scanf("%s",iii );
+    const char *input =iii;
+    // const char *input ="1+(2*(3+4*5-(6-7))-8)";//= "1+2*(3-4)";
+    // scanf("%s",input );
+    //1+(2*(3+4*5-(6-7))-8)
+    char output[128];
+
+    if(shunting_yard(input, output))    {
+
+        printf("output: %s\n", output);
+    }
+    return 0;
+}
