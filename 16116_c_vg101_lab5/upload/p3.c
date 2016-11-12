bb1e9e867bec824d115061229926cd6b527527f1
Ren Wang <iamwrm@gmail.com>
2016-11-12 10:23:51 +0800

    p3

--- 16116_c_vg101_lab5/upload/p3.c
+++ 16116_c_vg101_lab5/upload/p3.c
@@ -1,186 +1,103 @@
+#include "stdio.h"
+#include "stdlib.h"
 #include "string.h"
-#include "string.h"
-#include <stdbool.h>
-// 操作符
-// 优先级		符号		运算顺序
-// 1		!		从右至左
-// 2		* / %		从左至右
-// 3		+ -		从左至右
-// 4		=		从右至左
-int op_preced(const char c)
-{
+
+char stack[32];
+
+int op_preced(char c){
     switch(c)    {
         case '*':  case '/':
-            return 3;
-        case '+': case '-':
-            return 2;
+            return 1;
+        default:
+            return 0;
     }
+}
+
+//1+(2*(3+4*5-(6-7))-8)
+
+int isOperater(char c){
+    if  (c == '+' || c == '-' || c == '/' || c == '*' )
+        return 1;
     return 0;
 }
 
-unsigned int op_arg_count(const char c)
-{
-            return 2;
+void print_and_change(char * outpos,char c){
+    *outpos = c; ++outpos;
+    *outpos=',';
+    ++outpos;
 }
 
-#define op_left_assoc(c) (c == '+' || c == '-' || c == '/' || c == '*' || c == '%')
-#define is_operator(c)   (c == '+' || c == '-' || c == '/' || c == '*' || c == '!' || c == '%' || c == '=')
-#define is_function(c)   (c >= 'A' && c <= 'Z')
-#define is_ident(c)      ((c >= '0' && c <= '9') || (c >= 'a' && c <= 'z'))
-
-bool shunting_yard(const char *input, char *output)
+int shuffle( char *input, char *output)
 {
-    const char *strpos = input, *strend = input + strlen(input);
-    char c, stack[32], sc, *outpos = output;
+    char *strpos = NULL;
+    strpos = input;
+    char c,sc;
+    char *outpos = NULL;
+    outpos=output;
     unsigned int sl = 0;
-    while(strpos < strend)   {
+    while(*strpos !='\0')   {
         c = *strpos;
         if(c != ' ')    {
-            // 如果输入为一个数字，则直接入输出队列
-            if(is_ident(c))  {
-                *outpos = c; ++outpos;
-                *outpos=',';
-                ++outpos;
+            if(c >= '0' && c <= '9')  {
+                print_and_change(outpos,c);
+                outpos+=2;
             }
-            // 如果输入符号为操作符，op1，然后:
-            else if(is_operator(c))  {
+            else if(isOperater(c))  {
                 while(sl > 0)    {
                     sc = stack[sl - 1];
-                    // While there is an operator token, o2, at the top of the stack
-                    // op1 is left-associative and its precedence is less than or equal to that of op2,
-                    // or op1 is right-associative and its precedence is less than that of op2,
-                    if(is_operator(sc) &&
-                        ((op_left_assoc(c) && (op_preced(c) <= op_preced(sc))) ||
-                           (!op_left_assoc(c) && (op_preced(c) < op_preced(sc)))))   {
-                        // Pop o2 off the stack, onto the output queue;
-                        *outpos = sc;
-                        ++outpos;
-                        *outpos=',';
-                        ++outpos;
+                    if(isOperater(sc) && ( (op_preced(c) <= op_preced(sc) )) )  {
+                        print_and_change(outpos,sc);
+                        outpos+=2;
                         sl--;
                     }
-                    else   {
-                        break;
-                    }
+                    else   break;
                 }
-                // push op1 onto the stack.
                 stack[sl] = c;
                 ++sl;
             }
-            // If the token is a left parenthesis, then push it onto the stack.
             else if(c == '(')   {
                 stack[sl] = c;
                 ++sl;
             }
-            // If the token is a right parenthesis:
             else if(c == ')')    {
-                bool pe = false;
-                // Until the token at the top of the stack is a left parenthesis,
-                // pop operators off the stack onto the output queue
+                int pe = 0;
                 while(sl > 0)     {
                     sc = stack[sl - 1];
                     if(sc == '(')    {
-                        pe = true;
+                        pe = 1;
                         break;
                     }
                     else  {
-                        *outpos = sc;
-                        ++outpos;
-                        *outpos=',';
-                        ++outpos;
+                        print_and_change(outpos,sc);
+                        outpos+=2;
                         sl--;
                     }
                 }
-                // Pop the left parenthesis from the stack, but not onto the output queue.
                 sl--;
-                // If the token at the top of the stack is a function token, pop it onto the output queue.
-
-
-
             }
         }
         ++strpos;
     }
-    // When there are no more tokens to read:
-    // While there are still operator tokens in the stack:
     while(sl > 0)  {
         sc = stack[sl - 1];
-        if(sc == '(' || sc == ')')   {
-            printf("Error: parentheses mismatched\n");
-            return false;
-        }
-        *outpos = sc;
-        ++outpos;
-        *outpos=',';
-        ++outpos;
-
+        print_and_change(outpos,sc);
+        outpos+=2;
         --sl;
     }
-    *outpos = 0; // Null terminator
-    return true;
-}
-
-bool execution_order(const char *input) {
-    const char *strpos = input, *strend = input + strlen(input);
-    char c, res[4];
-    unsigned int sl = 0, sc, stack[32], rn = 0;
-	// While there are input tokens left
-    while(strpos < strend)  {
-		// Read the next token from input.
-        c = *strpos;
-		// If the token is a value or identifier
-        if(is_ident(c))    {
-			// Push it onto the stack.
-            stack[sl] = c;
-            ++sl;
-        }
-		// Otherwise, the token is an operator  (operator here includes both operators, and functions).
-        else if(is_operator(c) || is_function(c))    {
-			++rn;
-			// It is known a priori that the operator takes n arguments.
-			unsigned int nargs = op_arg_count(c);
-			// If there are fewer than n values on the stack
-			if(sl < nargs) {
-				// (Error) The user has not input sufficient values in the expression.
-				return false;
-			}
-			// Else, Pop the top n values from the stack.
-			// Evaluate the operator, with the values as arguments.
-			// Push the returned results, if any, back onto the stack.
-            stack[sl] = *(unsigned int*)res;
-            ++sl;
-        }
-        ++strpos;
-    }
-	// If there is only one value in the stack
-	// That value is the result of the calculation.
-	if(sl == 1) {
-		sc = stack[sl - 1];
-		sl--;
-	//	printf("%s is a result\n", &sc);
-		return true;
-	}
-	// If there are more values in the stack
-	// (Error) The user input has too many values.
-	return false;
+    *outpos = 0;
+    return 1;
 }
 
 int main() {
-    // functions: A() B(a) C(a, b), D(a, b, c) ...
-    // identifiers: 0 1 2 3 ... and a b c d e ...
-    // operators: = - + / * % !
+
     char iii[100];
     printf("input: ");
     scanf("%s",iii );
-    const char *input =iii;
-    // const char *input ="1+(2*(3+4*5-(6-7))-8)";//= "1+2*(3-4)";
-    // scanf("%s",input );
-    //1+(2*(3+4*5-(6-7))-8)
+
+    //  1+(2*(3+4*5-(6-7))-8)
+
     char output[128];
-
-    if(shunting_yard(input, output))    {
-
-        printf("output: %s\n", output);
-    }
+    shuffle(iii, output);
+    printf("output: %s\n", output);
     return 0;
 }
