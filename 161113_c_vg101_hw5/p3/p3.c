2f2b0bb7c8a99a5f40cf7326bf45aad10a0607a5
Ren Wang <iamwrm@gmail.com>
2016-11-13 18:21:14 +0800

    hw5 完成

--- 161113_c_vg101_hw5/p3/p3.c
+++ 161113_c_vg101_hw5/p3/p3.c
@@ -1,6 +1,6 @@
 /* GNU gcc compiler on windows 10 x64 platform
-mingw32  gcc version 4.9.2
- -std=c11					*/
+mingw32  g++ version 4.9.2
+ -std=c++11					*/
 
 #include "stdio.h"
 #include "stdlib.h"
@@ -26,10 +26,14 @@ void reverse(char *array, int n)
 
 int main()
 {
-    char s1[260] = "12345";
-    char s2[260] = "11232345";
-    // scanf("%s", s1);
-    // scanf("%s", s2);
+    char s1[260];
+    char s2[260];
+
+    printf(" first integer? ");
+    scanf("%s", s1);
+    printf("second integer? ");
+    scanf("%s", s2);
+
 
     int i = 0;
     int n_s1 = 0;
@@ -43,12 +47,48 @@ int main()
 
     reverse(s1, n_s1);
     reverse(s2, n_s2);
-    printf("s1: %s\n", s1);
-    printf("s2: %s\n", s2);
+
+    for (i = n_s1; i < 260; i++)
+        s1[i] = '0';
+    for (i = n_s2; i < 260; i++)
+        s2[i] = '0';
+
+    int sum[260];
+    for (i = 0; i < 260; i++)
+    {
+        sum[i] = s1[i] + s2[i] - '0' - '0';
+    }
+
+    for (i = 0; i < 260; i++)
+    {
+        if (sum[i] >= 10)
+        {
+            sum[i] -= 10;
+            sum[i + 1] += 1;
+        }
+    }
 
 
+    int j;
+    for (j = 0; j < 260; j++)
+    {
+        if (sum[260 - j] != 0)
+            break;
+    }
+    j = 260 - j + 1;
 
 
+    char result[260];
+
+    for (i = 0; i < j; i++)
+    {
+        result[i] = sum[i] + '0';
+    }
+    result[i] = '\0';
+    reverse(result, i);
+    printf("Their sum is: %s", result);
+
+    getchar();
     getchar();
     return 0;
 }
