3c5d5064f9d69631557b20b41cae2511fa92dedc
Ren Wang <iamwrm@gmail.com>
2016-11-14 17:57:23 +0800

    完成了plot函数

--- 161114_c_vg101_lab6/test.c
+++ 161114_c_vg101_lab6/test.c
@@ -6,10 +6,64 @@ mingw32  gcc version 4.9.2
 #include "stdlib.h"
 #include "math.h"
 
+void plot_board(int *points);
+
 int main()
 {
-
+    int points[64]; // empty is 0;  white 0 is 1; black x is 2
+    for (int i = 0; i < 64; i++)
+        points[i] = 0;
     
+    points[27] = 1;
+    points[28] = 2;
+    points[27+9] = 1;
+    points[28+7] = 2;
+
+
+    plot_board(points);
     getchar();
     getchar();
+}
+
+void plot_board(int *points)
+{
+
+    //print the above line --------------
+    printf(" ");
+    char abc[8] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'};
+    for (int i = 0; i < 8; i++)
+        printf("  %c ", abc[i]);
+    printf("\n");
+    printf(" +");
+    for (int i = 0; i < 8; i++)
+        printf("---+");
+    printf("\n");
+    //------------------
+
+    //the first line-----
+
+    for (int j = 0; j < 8; j++)
+    {
+        printf("%c|",j+1+'0');
+        for (int i = 0; i < 8; i++)
+        {
+            switch (points[i+8*j])
+            {
+            case 0:
+                printf("   |");
+                break;
+            case 1:
+                printf(" o |");
+                break;
+            case 2:
+                printf(" x |");
+                break;
+            }
+        }
+        printf("\n");
+        printf(" +");
+        for (int i = 0; i < 8; i++)
+            printf("---+");
+        printf("\n");
+    }
 }
