3d92a5494e9f486a7ecabacba9f9bcd4e8be2e6f
Ren Wang <iamwrm@gmail.com>
2016-11-14 17:13:42 +0800

    正式开始lab6

---
+++ 161114_c_vg101_lab6/test.c
@@ -0,0 +1,15 @@
+/* GNU gcc compiler on windows 10 x64 platform
+mingw32  gcc version 4.9.2
+ -std=c11					*/
+
+#include "stdio.h"
+#include "stdlib.h"
+#include "math.h"
+
+int main()
+{
+
+    
+    getchar();
+    getchar();
+}
