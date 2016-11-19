1228ab46d53b85b319d779c627b267771347f230
Ren Wang <iamwrm@gmail.com>
2016-11-19 19:26:44 +0800

    学习了华为c语言风格规范，开始写汉诺塔递归

---
+++ 16115_c_Algorithm/hanoi/test1.c
@@ -0,0 +1,21 @@
+/* GNU gcc compiler on windows 10 x64 platform
+mingw32  gcc version 4.9.2
+ -std=c++90					
+ MIT License Copyright (c) 2016 iamwrm
+ <author>           <version>      <date>  
+ Wang, Ren          1.0             2016-11-19
+ 
+Description: 实现汉诺塔
+keywords:   递归 c 汉诺塔
+
+ */
+
+#include "stdio.h"
+#include "stdlib.h"
+#include "math.h"
+#include <string.h>
+
+int main()
+{
+    return 0;
+}
