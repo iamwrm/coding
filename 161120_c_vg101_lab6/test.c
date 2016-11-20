b2d836cadc5dbb2901c3c8efda8b9cf8f41b0d00
Ren Wang <iamwrm@gmail.com>
2016-11-20 23:18:05 +0800

    改了致命性的x,y 搞反错误

--- 161120_c_vg101_lab6/test.c
+++ 161120_c_vg101_lab6/test.c
@@ -19,51 +19,49 @@ keywords:   文件操作
 
 // 输出文件前的字符数组的长宽
 #define FILE_ROW 50
-#define FILE_COL 20
+#define FILE_COL 100
 
-void plot_one_cube(int x, int y, char arr[][FILE_ROW])
+void plot_one_cube(int x, int y, char arr[][FILE_COL])
 {
 
-    // char *arr_local;
-    // arr_local = arr + position;
-
+    
     // line 0
     const int width = 6;
     for (int i = 0; i < width + 1; i++)
     {
-        arr[x][y + i] = '_';
+        arr[y][x + i] = '_';
     }
 
     //line 1~3
     for (int dev = 0; dev < 3; dev++)
     {
 
-        arr[x + 1 + dev][y - 1 - dev] = '/';
+        arr[y + 1 + dev][x - 1 - dev] = '/';
         for (int i = 0; i < width; i++)
         {
             if (dev == 3 - 1)
-                arr[x + 1 + dev][y + i - dev] = '_';
+                arr[y + 1 + dev][x + i - dev] = '_';
             else
-                arr[x + 1 + dev][y + i - dev] = ' ';
+                arr[y + 1 + dev][x + i - dev] = ' ';
         }
-        arr[x + 1 + dev][y + width - dev] = '/';
-        arr[x + 1 + dev][y + width + 1] = '|';
+        arr[y + 1 + dev][x + width - dev] = '/';
+        arr[y + 1 + dev][x + width + 1] = '|';
     }
 
     //line 4~6
     for (int dev = 3; dev < 6; dev++)
     {
 
-        arr[x + 1 + dev][y - 3] = '|';
+        arr[y + 1 + dev][x - 3] = '|';
         for (int i = 0; i < width; i++)
         {
             if (dev == 6 - 1)
-                arr[x + 1 + dev][y - 3 + 1 + i] = '_';
+                arr[y + 1 + dev][x - 3 + 1 + i] = '_';
             else
-                arr[x + 1 + dev][y - 3 + 1 + i] = ' ';
+                arr[y + 1 + dev][x - 3 + 1 + i] = ' ';
         }
-        arr[x + 1 + dev][y + width - 2] = '|';
-        arr[x + 1 + dev][y + width + 1 - dev + 3] = '/';
+        arr[y + 1 + dev][x + width - 2] = '|';
+        arr[y + 1 + dev][x + width + 1 - dev + 3] = '/';
     }
 }
 
@@ -74,10 +72,10 @@ int main()
     fp = fopen("testout.txt", "w+");
 
     //初始化输出文件前的字符数组
-    char arr[FILE_COL][FILE_ROW];
-    for (int i = 0; i < FILE_COL; i++)
+    char arr[FILE_ROW][FILE_COL];
+    for (int i = 0; i < FILE_ROW; i++)
     {
-        for (int j = 0; j < FILE_ROW; j++)
+        for (int j = 0; j < FILE_COL; j++)
         {
             arr[i][j] = ' ';
         }
@@ -87,11 +85,14 @@ int main()
         0, 0, 0, 1, 3, 1, 0, 0, 0,
     }; //matrix input
 
-    plot_one_cube(5, 5, arr);
+    
+    plot_one_cube(15, 15, arr);
+    plot_one_cube(15, 12, arr);
 
-    for (int i = 0; i < FILE_COL; i++)
+    // 将数组写入txt
+    for (int i = 0; i < FILE_ROW; i++)
     {
-        for (int j = 0; j < FILE_ROW; j++)
+        for (int j = 0; j < FILE_COL; j++)
         {
             fprintf(fp, "%c", arr[i][j]);
         }
