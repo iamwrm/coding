fb0e61b19f284d497846051304b7897c3f9b5cfe
Ren Wang <iamwrm@gmail.com>
2016-11-24 22:54:32 +0800

    链表

--- 161124_cpp_lecture/main.cpp
+++ 161124_cpp_lecture/main.cpp
@@ -3,8 +3,18 @@
 
 using namespace std;
 
+typedef struct node_struct
+{
+    double value;
+    struct node_struct * prev;
+    struct node_struct * next;
+}node;
+
 int main()
 {
+    
+    
+    
     cout<<"heheheh"<<endl;
     system("pause");
 }
