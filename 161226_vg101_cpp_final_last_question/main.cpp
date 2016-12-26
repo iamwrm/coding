6fdd2eb5985ebceb0efcaa20de32d90b3cad002a
Ren Wang <iamwrm@gmail.com>
2016-12-26 12:35:51 +0800

    part 1 finish

--- 161226_vg101_cpp_final_last_question/main.cpp
+++ 161226_vg101_cpp_final_last_question/main.cpp
@@ -1,7 +1,45 @@
 #include <iostream>
 
+using namespace std;
+
+
+std::string uint8ToBin(int x);
+
+
 int main()
 {
-    std::cout << "Hello, World!" << std::endl;
+    int x = 233;
+    string hehe = uint8ToBin(x);
+    cout << hehe;
     return 0;
+
+}
+
+
+std::string uint8ToBin(int x)
+{
+// 0<=x<=255
+    string s1("00000000");
+    int temp = x;
+    for (int i = 0; temp > 0; i++)
+    {
+        if (temp % 2 == 1)
+        {
+            if (7 - i < 0)
+            {
+                cout << "error! 7-i<0\n";
+                s1 = "NaN";
+                break;
+
+            }
+            s1[7 - i] = '1';
+            temp = (temp - 1) / 2;
+        }
+        else
+        {
+            temp /= 2;
+        }
+
+    }
+    return s1;
 }
