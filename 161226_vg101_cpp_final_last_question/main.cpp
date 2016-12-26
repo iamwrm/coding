6e63f3b716d9dc210be59fdecfbd902777c7257e
Ren Wang <iamwrm@gmail.com>
2016-12-26 16:08:34 +0800

    Q2 finish

--- 161226_vg101_cpp_final_last_question/main.cpp
+++ 161226_vg101_cpp_final_last_question/main.cpp
@@ -1,18 +1,39 @@
 #include <iostream>
+#include <fstream>
+#include <vector>
+#include <string>
+
 
 using namespace std;
 
 
 std::string uint8ToBin(int x);
 
+std::string FileToString(std::string sFileName);
+
 
 int main()
 {
-    int x = 233;
-    string hehe = uint8ToBin(x);
-    cout << hehe;
-    return 0;
 
+// Test uint8ToBin
+//    int x;
+//    cout << "Please input a number among [0 255]: ";
+//    cin >> x;
+//    cout << uint8ToBin(x) << endl;
+
+
+// Test FileToString
+    string sCode = FileToString("P5-Encodedfile.dat");
+    cout << sCode << endl;
+
+
+
+
+// Test Decode
+//    cout << Decode(sCode) << endl;
+
+
+    return 0;
 }
 
 
@@ -43,3 +64,44 @@ std::string uint8ToBin(int x)
     }
     return s1;
 }
+
+std::string FileToString(std::string sFileName)
+{
+// f77a d00a 070a
+    fstream f_in_b(sFileName, ios::binary | ios::in);
+    string file_string;
+
+    std::vector<int> v1;
+
+    int temp;
+
+
+    while (f_in_b.read(reinterpret_cast<char * >(&temp ), 1))
+    {
+        v1.push_back(temp);
+    }
+
+    string converted = "";
+
+    int i = 0;
+    for (i = 0; i < v1.size() - 2; i++)
+    {
+        string temps;
+
+        temps = uint8ToBin(v1[i]);
+        converted = converted + temps;
+
+    }
+
+    string l1;
+    l1 = uint8ToBin(v1[i++]);
+
+    int n_in_last_string = v1[v1.size() - 1];
+
+    string l1p;
+    l1p = l1.substr(8 - n_in_last_string);
+
+    converted = converted + l1p;
+    return converted;
+}
+
