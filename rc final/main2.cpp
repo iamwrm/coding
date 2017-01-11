6c8004792f1a849a1f73086f88caca56ca515489
Ren Wang <iamwrm@gmail.com>
2017-01-11 18:39:17 +0800

    Org notes

--- rc final/main2.cpp
+++ rc final/main2.cpp
@@ -1,33 +1,59 @@
+#include <algorithm>
+#include <cstdlib>
 #include <iostream>
+#include <sstream>
 #include <string>
 #include <vector>
-#include <algorithm>
-#include <sstream>
-#include <cstdlib>
 
 using namespace std;
 
-int main(int argc, char const *argv[])
+class mycom
 {
-	string line, word;
-	getline(cin, line);
-	stringstream stream(line);
-	vector<string> v;
-	while (stream >> word)
-	{
-		
-		cout << word<<endl;
-		v.push_back(word);
-	}
-	sort(v.begin(), v.end());
 
+    double Im;
+    double Re;
 
-	for (int  i = 0; i < v.size(); i++)
-	{
-		cout << v[i]<<" ";
+  public:
+    mycom()
+    {
+        Im = Re = 0;
+    }
+    void show()
+    {
+        cout << Re << " " << Im << endl;
+    }
+    void set(double i1, double i2)
+    {
+        Re = i1;
+        Im = i2;
+    }
+    void get(double &i1, double &i2)
+    {
+        i1 = Re;
+        i2 = Im;
+    }
+};
 
-	}
+int main()
+{
+    string line, word;
+    getline(cin, line);
+    stringstream stream(line);
+    vector<string> v;
+    while (stream >> word)
+    {
+        cout << word << endl;
+        v.push_back(word);
+    }
+    sort(v.begin(), v.end());
 
-	system("pause");
-	return 0;
+    for (int i = 0; i < v.size(); i++)
+    {
+        cout << v[i] << " ";
+    }
+
+    mycom mmm1;
+    mmm1.set(1, 2);
+
+    return 0;
 }
