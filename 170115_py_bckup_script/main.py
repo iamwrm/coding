260ba115f7750cac9fa67e4c3e36d0e1dd85e092
Ren Wang <iamwrm@gmail.com>
2017-01-16 09:46:16 +0800

    Python backup script

---
+++ 170115_py_bckup_script/main.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+
+import zipfile
+import os
+import datetime
+
+def my_backup(filename):
+    current_time=str(datetime.datetime.now())
+    cc=current_time
+
+    back_up_dir="/Users/wangren/Box Sync/spacemacs-bkp/backup_python/"
+    back_up_file=back_up_dir + cc[0:19] +".zip"
+    backupZIP=zipfile.ZipFile(back_up_file,'a')
+
+    backupZIP.write(filename)
+
+    backupZIP.close()
+
+
+
+my_backup("/Users/wangren/.spacemacs")
+my_backup("/Users/wangren/.emacs.d/init.el")
+
+
+
---
+++ 170115_py_bckup_script/tt.py
@@ -0,0 +1,7 @@
+import datetime
+
+current_time=datetime.datetime.now()
+current_time_str=str(current_time)
+
+print(current_time_str)
+
