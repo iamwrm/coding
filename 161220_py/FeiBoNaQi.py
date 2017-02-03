2be14948d70b7cd3a36f6a893155fc144eb9efae
Ren Wang <iamwrm@gmail.com>
2017-02-03 23:39:48 +0800

    indent problem

--- 161220_py/FeiBoNaQi.py
+++ 161220_py/FeiBoNaQi.py
@@ -1,8 +1,5 @@
 from functools import reduce
-							
-	def char2num(s):
+
+
+def char2num(s):
     return {'0': 0, '1': 1, '2': 2, '3': 3, '4': 4, '5': 5, '6': 6, '7': 7, '8': 8, '9': 9}[s]
-
-
-
-
