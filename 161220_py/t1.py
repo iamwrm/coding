16ddc4a4710c0944fd07bb3129d2ad6e09ea2875
Ren Wang <iamwrm@gmail.com>
2017-02-04 09:29:13 +0800

    create a gitignore for scratch

--- 161220_py/t1.py
+++ 161220_py/t1.py
@@ -3,8 +3,9 @@
 
 
 def bubble_sort(arr, head, end):
-    for index in range(head, end)
-    print(arr[index])
+    for index in range(head, end):
 
 
 arr = [1, 2, 3, 4, 3, 2, 1, 5, 3, 2, 4, 5, 3, 2, 2]
+
+bubble_sort(arr, 2, 4)
