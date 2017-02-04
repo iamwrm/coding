89770492357e54ed76b0c04395f336aac2aa25fd
Ren Wang <iamwrm@gmail.com>
2017-02-04 09:06:31 +0800

    reindent

--- 161220_py/try_py_for.py
+++ 161220_py/try_py_for.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-arr=['aaaaaa','bbbbbb',123,234234];
+arr = ['aaaaaa', 'bbbbbb', 123, 234234]
 for a in arr:
-	print(a*2);
-
+    print(a * 2)
--- 170115_py_bckup_script/main.py
+++ 170115_py_bckup_script/main.py
@@ -4,23 +4,20 @@ import zipfile
 import os
 import datetime
 
-def my_backup(filename):
-    current_time=str(datetime.datetime.now())
-    cc=current_time
 
-    back_up_dir="/Users/wangren/Box Sync/spacemacs-bkp/backup_python/"
-    back_up_file=back_up_dir + cc[0:19] +".zip"
-    backupZIP=zipfile.ZipFile(back_up_file,'a')
+def my_backup(filename):
+    current_time = str(datetime.datetime.now())
+    cc = current_time
+
+    back_up_dir = "/Users/wangren/Box Sync/spacemacs-bkp/backup_python/"
+    back_up_file = back_up_dir + cc[0:19] + ".zip"
+    backupZIP = zipfile.ZipFile(back_up_file, 'a')
 
     backupZIP.write(filename)
 
     backupZIP.close()
-    print(filename+" backup into "+back_up_file+" succeeded!")
-
+    print(filename + " backup into " + back_up_file + " succeeded!")
 
 
 my_backup("/Users/wangren/.spacemacs")
 my_backup("/Users/wangren/.emacs.d/init.el")
-
-
-
