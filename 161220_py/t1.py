c29f3603b566766f0184da7580cd8b0b08b3359e
Ren Wang <iamwrm@gmail.com>
2017-02-04 09:14:55 +0800

    try to write a bubble sort

---
+++ 161220_py/t1.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+
+def bubble_sort(arr, head, end):
+    for index in range(head, end)
+    print(arr[index])
+
+
+arr = [1, 2, 3, 4, 3, 2, 1, 5, 3, 2, 4, 5, 3, 2, 2]
