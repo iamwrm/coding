668e6b2c083ad2b222ab07a157c60db500fcbbf8
Ren Wang <iamwrm@gmail.com>
2017-02-23 09:06:06 +0800

    succeeded in using vector to store args

--- 17_c_terminal_arg/main.cpp
+++ 17_c_terminal_arg/main.cpp
@@ -6,7 +6,6 @@ using namespace std;
 int main(int argc, char const *argv[])
 {
     std::cout << "argc:" << argc << std::endl;
-    std::cout << "hehhehehehehe\n";
     vector<string> v1(argc);
     for (int i = 0; i < argc; i++)
     {
@@ -14,7 +13,9 @@ int main(int argc, char const *argv[])
         int j = 0;
         while (*(*(argv + i) + j) != '\0')
         {
-            temp.push_back(*(*(argv + i) + j) != '\0');
+            temp.push_back(*(*(argv + i) + j));
+            //cout << *(*(argv + i) + j);
+            j++;
         }
         cout << temp << endl;
         v1.push_back(temp);
