9cc9f368496fdced9a3bda7cf1459ee86c7a6ecc
Ren Wang <iamwrm@gmail.com>
2017-02-23 09:01:25 +0800

    using vector

--- 17_c_terminal_arg/main.cpp
+++ 17_c_terminal_arg/main.cpp
@@ -1,11 +1,23 @@
 #include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
 
 int main(int argc, char const *argv[])
 {
     std::cout << "argc:" << argc << std::endl;
-    std::cout << "hehhehehehehe\n"
-              << **(argv + 1)
-              << *(*argv + 1)
-              << *(*argv + 2);
+    std::cout << "hehhehehehehe\n";
+    vector<string> v1(argc);
+    for (int i = 0; i < argc; i++)
+    {
+        string temp;
+        int j = 0;
+        while (*(*(argv + i) + j) != '\0')
+        {
+            temp.push_back(*(*(argv + i) + j) != '\0');
+        }
+        cout << temp << endl;
+        v1.push_back(temp);
+    }
     return 0;
 }
